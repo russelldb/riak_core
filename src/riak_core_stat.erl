357fdef25e31b05f9b4b30f0797df074777fd69f
Russell Brown <russell.brown@mac.com>
2012-05-03 12:02:26 -0400

    Start stat path parsing

--- src/riak_core_stat.erl
+++ src/riak_core_stat.erl
@@ -24,7 +24,7 @@
 -export([get_stats/0, get_stats/1, update/1]).
 
 %% query API
--export([list_stats/0, stats/1, stats/2, stats/3, stat/2, stat/3]).
+-export([list_stats/0, stats/1, stats/2, stats/3, stat/2, stat/3, path/2, parse_path/1]).
 
 %% Metrics API
 -export([stat_specs/0]).
@@ -91,6 +91,23 @@ stat(Name, Level) ->
 stat(Name, Level, Spec) ->
     riak_core_metric_proc:value(Name, Level, Spec).
 
+%% Level must be parsed out by
+%% code that feeds in
+%% Path is a list of tokens
+%% strings / atoms / binaries
+%% Attempts to resolve Path down to a stat / set of stats to display
+path(Level, []) ->
+    stats(Level);
+path(Level, Path) ->
+    case parse_path(Path) of
+        {undefined, _, Name, Spec} ->
+            stat(Name, Level, Spec);
+        {App, undefined, _, _} ->
+            stats(App, Level);
+        {App, Group, _, _} ->
+            stats(App, Group, Level)
+    end.
+
 %% @spec get_stats() -> proplist()
 %% @doc Get the current aggregation of stats.
 get_stats() ->
@@ -129,4 +146,40 @@ update(_) ->
 %% @doc Produce a proplist-formatted view of the current aggregation
 %%      of stats.
 produce_stats(Level) ->
-    lists:flatten([ riak_core_metric_proc:value(Name, Level) || {Name, _} <- stat_specs() ]).
+    lists:flatten([riak_core_metric_proc:value(Name, Level) || {Name, _} <- stat_specs() ]).
+
+parse_path(Path) ->
+    parse_path(list_stats(), Path).
+
+parse_path(Stats, []) ->
+    Stats;
+parse_path(Stats0, [Hd|Tl]) ->
+    {App, Group, Stats, Path} = case is_member(Hd, Stats0) of
+                                    false ->
+                                        {undefined, undefined, Stats0, Tl};
+                                    {true, App0, AppStats} ->
+                                        MaybeGrp = hd(Tl),
+                                        case is_member(MaybeGrp, AppStats) of
+                                            {true, Group0, GroupStats} -> {App0, Group0, GroupStats, tl(Tl)};
+                                            false -> {App0, undefined, AppStats, Tl}
+                                        end
+
+                                end,
+    {App, Group, Stats, Path}.
+
+%% see if the token is an app in the stat list
+is_member(MaybeMem, Stats) when is_list(MaybeMem) ->
+    is_member(MaybeMem, Stats, fun(X) -> atom_to_list(X) end, fun(X) -> list_to_existing_atom(X) end);
+is_member(MaybeMem, Stats) when is_binary(MaybeMem) ->
+    is_member(MaybeMem, Stats, fun(X) -> atom_to_binary(X, latin1) end, fun(X) -> binary_to_existing_atom(X, latin1) end);
+is_member(MaybeMem, Stats) when is_atom(MaybeMem) ->
+    is_member(MaybeMem, Stats, fun(X) -> X  end, fun(X) -> X end).
+
+is_member(MaybeMem, Stats, InConv, OutConv) ->
+    case lists:member(MaybeMem, [InConv(Mem) || {Mem, _} <- Stats]) of
+        true ->
+            OutMem = OutConv(MaybeMem),
+            {true, OutMem, proplists:get_value(OutMem, Stats)};
+        false ->
+            false
+    end.
